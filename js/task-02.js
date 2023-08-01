a653d6161dcae25d2b08c5b74d07a995117f70c7
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-01 14:37:34 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,17 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 const liEl = document.querySelector("#ingredients");
 
 const createList = ingredients.map((element) => {
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.textContent = element;
   li.classList.add("item");
-  return li;});
+  return li;
+});
 
-  liEl.append(...createList);
+liEl.append(...createList);
