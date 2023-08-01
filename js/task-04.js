95ca06adda723a9d09e7a099521ab03b9bf2622b
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-01 14:37:41 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,17 @@
-
 const decBtn = document.querySelector('button[data-action="decrement"]');
-const incBtn = document.querySelector('button[data-action="increment"]') ;
-const counter = document.querySelector('#value');
+const incBtn = document.querySelector('button[data-action="increment"]');
+const counter = document.querySelector("#value");
 
 let counterValue = 0;
 
-decBtn.addEventListener('click', decCounter)
-incBtn.addEventListener('click', incCounter)
+decBtn.addEventListener("click", decCounter);
+incBtn.addEventListener("click", incCounter);
 
-
-function decCounter(event){ counterValue -=1; 
-counter.textContent = counterValue};
-function incCounter(event){ counterValue +=1; 
-counter.textContent = counterValue};
-
-
-
+function decCounter(event) {
+  counterValue -= 1;
+  counter.textContent = counterValue;
+}
+function incCounter(event) {
+  counterValue += 1;
+  counter.textContent = counterValue;
+}
