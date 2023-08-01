b9972e7fa497ee8600b615c1f3491ed51c56e2e9
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-01 14:31:53 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#validation-input');
+
+input.addEventListener('blur', inputValid);
+
+function inputValid(event) { 
+    if ( event.currentTarget.value.length === Number(input.dataset.length))
+     {input.classList.add('valid');
+    input.classList.remove('invalid')}
+    else {input.classList.remove('valid');
+    input.classList.add('invalid');
+}
+    }
