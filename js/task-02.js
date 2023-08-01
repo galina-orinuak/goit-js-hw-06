4e73e0e344e74814c647125eee36bfdfe221bd86
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-01 14:31:42 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const liEl = document.querySelector("#ingredients");
+
+const createList = ingredients.map((element) => {
+  const li = document.createElement('li');
+  li.textContent = element;
+  li.classList.add("item");
+  return li;});
+
+  liEl.append(...createList);
