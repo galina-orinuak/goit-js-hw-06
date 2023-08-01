e9aa3b91e2f64a61d1db0bc16eeffec5e4c321e1
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-01 14:37:44 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,11 @@
-const inputName = document.querySelector('#name-input');
-const outputName = document.querySelector('#name-output');
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
 
-inputName.addEventListener('input', handlerName);
+inputName.addEventListener("input", handlerName);
 
 function handlerName(event) {
-    outputName.textContent = event.currentTarget.value.trim();
-    if (event.currentTarget.value.trim() === ""){outputName.textContent = "Anonymous";}
+  outputName.textContent = event.currentTarget.value.trim();
+  if (event.currentTarget.value.trim() === "") {
+    outputName.textContent = "Anonymous";
+  }
 }
-
