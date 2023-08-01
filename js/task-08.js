05cf8c838b60a0d44ed7e1bedfeea2034fc88560
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-01 14:37:56 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,19 @@
 const elements = {
-    form: document.querySelector('.login-form')}
-
-elements.form.addEventListener('submit', handlerSubmit);
-
-function handlerSubmit(event){
-    event.preventDefault();
-
-    const {elements: { email, password }} = event.currentTarget;
-
-    console.log({Email:email.value, Password:password.value});
-
-    if (email.value==="" || password.value==="") { alert ('Всі поля повинні бути заповнені')}
-    
+  form: document.querySelector(".login-form"),
 };
 
+elements.form.addEventListener("submit", handlerSubmit);
 
+function handlerSubmit(event) {
+  event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  console.log({ Email: email.value, Password: password.value });
+
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені");
+  }
+}
