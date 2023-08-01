bdafacf458d0eebe2958515305c32b57e8d833bc
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-01 14:31:38 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const items = document.querySelectorAll('.item');
+console.dir ('Number of categories: ${items.length}');
+
+items.forEach(function(element) {
+    console.log(`Category: ${element.firstElementChild.textContent}`),
+    console.log(`Elements: ${element.lastElementChild.children.length}`)
+});
