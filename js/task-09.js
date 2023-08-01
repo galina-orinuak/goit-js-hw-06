5571572c723a5977292b4f8af2b3bbc6f272ee83
kruhitka1706 <kruhitka1706@gmail.com>
2023-08-01 14:38:00 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,18 +1,16 @@
-const btn = document.querySelector('.change-color');
-const span = document.querySelector('.color');
-const body = document.querySelector('body')
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+const body = document.querySelector("body");
 
-btn.addEventListener('click', click)
+btn.addEventListener("click", click);
 
-function click(event){
+function click(event) {
   let rndmColor = getRandomHexColor();
   body.style.background = rndmColor;
- span.textContent = rndmColor;
+  span.textContent = rndmColor;
 }
 
-
 function getRandomHexColor() {
-  
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
